d1d660a38e199ee40c858d20c4e797133e37240b
mxd <mxd789@gmail.com>
2017-03-18 23:38:21 -0400

    set up 'F1: Menu' && 'CSCI 1730 EDITOR' Text

--- p2.cpp
+++ p2.cpp
@@ -3,8 +3,12 @@
 int main()
 {
 	initscr();// #0
-	addstr("F1 Menu");//#2
-	printw("CSCI 1730 Editor!");// #0
+	mvaddstr(0,0,"F1: Menu");//#0 && #2
+	mvaddstr(0,COLS/2,"CSCI 1730 Editor!");// #0
+
+	
+	//KEY_F(1); //#2
+
 	refresh();// #0
 	getch();  // #0
 	endwin(); // #0
@@ -15,4 +19,5 @@ Development Steps
 #0 initial ncurses window
 #1 Editor TUI
 #2 F1 Menu
+#3 Extra Credit
 */
