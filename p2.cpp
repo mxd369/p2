697f0496e99a797cf5d061ccc72932631a1584f1
mxd <mxd789@gmail.com>
2017-03-18 23:21:01 -0400

    initial ncurses window created, experimenting with menu

--- p2.cpp
+++ p2.cpp
@@ -2,10 +2,16 @@
 
 int main()
 {
-	initscr();
-	printw("CSCI 1730 Editor!");
-	refresh();
-	getch();
-	endwin();
-	return 0;
+	// #0
+	initscr();// #0
+	printw("CSCI 1730 Editor!");// #0
+	refresh();// #0
+	getch();// #0
+	endwin();// #0
+	return 0;// #0
 }
+/*
+Development Steps
+#0 initial ncurses window
+
+*/
