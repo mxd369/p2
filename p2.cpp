c10ee9772e1110736af07052f5324a7bf4666fe8
mxd <mxd789@gmail.com>
2017-03-18 23:27:39 -0400

    organized task into #0 The scaffold, #1 Editor TUI, #2 F1 Menu, #3 Extra Credit

--- p2.cpp
+++ p2.cpp
@@ -2,16 +2,17 @@
 
 int main()
 {
-	// #0
 	initscr();// #0
+	addstr("F1 Menu");//#2
 	printw("CSCI 1730 Editor!");// #0
 	refresh();// #0
-	getch();// #0
-	endwin();// #0
-	return 0;// #0
+	getch();  // #0
+	endwin(); // #0
+	return 0; // #0
 }
 /*
 Development Steps
 #0 initial ncurses window
-
+#1 Editor TUI
+#2 F1 Menu
 */
