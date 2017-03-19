4c254621dbc88d630dca17b3c023696ce049f733
mxd <mxd789@gmail.com>
2017-03-19 19:07:54 -0400

    added paddy1-3, added subpad in testdir, tried to add 'menubar' functionality but failed so commented out, need to create a branch for testing

--- p2.cpp
+++ p2.cpp
@@ -8,15 +8,54 @@ Development Steps
 #3 Extra Credit
 */
 
-
-void showF1Menu(void);// #2
+// Global Variables
 
 WINDOW *F1Menu;// #2
+int COL2 = 38;	//#1 variable for COL2, testing mvaddstr
+//int MENUMAX = 6;//#2 options for the menu **NOT WORKING
 
-int COL2 = 38;//#1 variable for COL2, testing mvaddstr
+/*
+ #2
+ drawmenu
+ needed to select menu items
+
+void drawmenu(int item)
+{
+	int c;
+	char mainmenu[] = "Main Menu";
+	char menu[MENUMAX][21] = {		// 6
+		"Open",
+		"Save",
+		"Save As",
+		"Poop",
+		"Icecream",
+		"Exit"
+	};
+
+	clear();
+	addstr(mainmenu);
+	for(c=0;c<MENUMAX;c++)
+	{
+		if( c==item )
+			attron(A_REVERSE);	// highlight selection
+		mvaddstr(3+(c*2),20,menu[c]);
+		attroff(A_REVERSE);		// remove highlight
+	}
+	mvaddstr(17,25,"Use arrow keys to move; Enter to select:");// enter functionality not designed
+	refresh();
+}
+*/
+
+
+// prototype: only needed since it is place before main
+void showF1Menu(void);// #2
 
 int main()
 {
+
+	int key;		//#2 key for the menu
+	int menuitem = 0;	//#2 menu items
+
 	int ch;// #2
 	initscr();// #0
 	
@@ -33,7 +72,16 @@ int main()
 	mvwaddstr(F1Menu,1,1,"Menu Screen");
 	mvwaddstr(F1Menu,3,1,"Press [ ~ ] to quit");
 	mvwaddstr(F1Menu,5,1,"Press [ENTER] to go back");
-	box(F1Menu,0,0);
+
+
+
+
+
+	box(F1Menu,0,0); // end BuildF1Menu
+
+
+
+
 
 	/* now start the program loop */
 	mvaddstr(0,0,"F1: Menu");//#1, testing mvaddstr
@@ -55,6 +103,37 @@ int main()
 		else
 			addch(ch);
 	} while (ch != '~');
+	
+	
+	
+	
+/*
+  Menu Items
+
+drawmenu(menuitem);
+keypad(stdscr,TRUE);
+noecho();			// Disable echo
+do
+{
+	key = getch();
+	switch(key)
+	{
+		case KEY_DOWN:
+			menuitem++;
+			if(menuitem > MENUMAX-1) menuitem = 0;
+			break;
+		case KEY_UP:
+			menuitem--;
+			if(menuitem < 0) menuitem = MENUMAX-1;
+			break;
+		default:
+			break;
+	}
+	drawmenu(menuitem);
+} while(key != '\n');
+
+echo();				/ re-enable echo 
+*/
 
 
 	refresh();// #0
